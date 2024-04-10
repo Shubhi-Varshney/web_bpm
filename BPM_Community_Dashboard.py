682d25993d8fae4b69cbc756c6ebab18f4230dcb
Shubhi Jain <email.shubhi.jain@gmail.com>
2024-04-10 13:44:14 +0200

    Changed bucket link

--- BPM_Community_Dashboard.py
+++ BPM_Community_Dashboard.py
@@ -34,7 +34,7 @@ alt.themes.enable("dark")
 # "cleaned_data_for_ml.csv"
 
 # https://console.cloud.google.com/storage/browser/bpm_bucket/cleaned_data_for_analysis.csv
-bucket_name = 'bpm_bucket'
+bucket_name = 'bpm_buckt'
 file_path_analytics = "cleaned_data_for_analysis.csv"
 file_path_ml = "cleaned_data_for_ml.csv"
 file_path_cg = "Community Growth.xlsx"
@@ -90,9 +90,9 @@ with st.sidebar:
 col_title = st.columns((1 ,6), gap="small")
 with col_title[1]:
     st.markdown('<span style="text-align: center; font-size: 50px; color: #000F40;">Berlin Product Managers |  Community Dashboard</span>', unsafe_allow_html=True)
-    
 
-    
+
+
 st.sidebar.markdown("# BPM Community Dashboard")
 
 
@@ -107,8 +107,8 @@ col = st.columns((3, 4, 2.5), gap='large',)
 with col[0]:
     st.markdown('<span style="font-size: 30px; color: #383971;">Event Attendance</span>', unsafe_allow_html=True)
      #st.markdown("<h1 style='text-align: center; color: red;'>Some title</h1>", unsafe_allow_html=True)
-           
-    
+
+
     event_mask = df_analytics["Event"] == selected_event
     df_event_masked = df_analytics[event_mask]
     df_event_status = df_event_masked["Attendee Status"].value_counts()
@@ -124,10 +124,10 @@ with col[0]:
     col2.metric("Venue Capacity", f"{venue_size}")
     col3.metric("Attandance Rate", f"{at_percent}%")
 
-st.markdown('#') 
-st.markdown('#') 
-st.markdown('#') 
-st.markdown('#') 
+st.markdown('#')
+st.markdown('#')
+st.markdown('#')
+st.markdown('#')
 
 with col[0]:
     st.markdown('<span style="font-size: 30px; color: #383971;">Participant Breakdown</span>', unsafe_allow_html=True)
@@ -135,14 +135,14 @@ with col[0]:
     mask = df_analytics["Event"] == selected_event
     df_analytics_masked = df_analytics[mask]
     df_job_position = pd.DataFrame(df_analytics_masked["Your Job Position"].value_counts().reset_index())
-    
+
     pie_colors =  ["#81D3C1", "#717c89","#8aa2a9","#90baad","#a1e5ab","#adf6b1", "#C1F9C4"]
-    
-    fig_pie = px.pie(df_job_position, values='count', names='Your Job Position', ) # 
+
+    fig_pie = px.pie(df_job_position, values='count', names='Your Job Position', ) #
     fig_pie.update_layout(showlegend=True, title= dict(text =str("Role"), font =dict(family="source sans pro", size=20, color = '#383971')))
     fig_pie.update_traces(hoverinfo='label+percent',
                   marker=dict(colors=pie_colors, ))
-  
+
     st.plotly_chart(fig_pie, use_container_width=True,sharing="streamlit", )
 
 
@@ -154,14 +154,14 @@ with col[0]:
 
 
    #  st.markdown('<span style="font-size: 30px; color: #383971;">Seniority Breakdown</span>', unsafe_allow_html=True)
-    
+
     pie_colors_2 =   ["#00C895","#39DEB6","#00B5B5", "#008AAB","#005F92",] # ["#005F92","#008AAB","#00B5B5", "#39DEB6","#00C895",]
-    
-    fig_pie_2 = px.pie(df_pod_list.iloc[:5], values='count', names='Choose your role', ) # 
+
+    fig_pie_2 = px.pie(df_pod_list.iloc[:5], values='count', names='Choose your role', ) #
     fig_pie_2.update_layout(showlegend=True, title= dict(text =str("Seniority"), font =dict(family="source sans pro", size=20, color = '#383971')))
     fig_pie_2.update_traces(hoverinfo='label+percent',
                   marker=dict(colors=pie_colors_2, ))
-  
+
     st.plotly_chart(fig_pie_2, use_container_width=True,sharing="streamlit", )
 
 
@@ -175,16 +175,16 @@ with col[1]:
     df_line['Socials'] = pd.to_datetime(df_line['Socials'], format='%d%b%Y:%H:%M:%S.%f')
     df_line['month'] = pd.DatetimeIndex(df_line['Socials']).month
     month_num = int(df_line['month'].iloc[(selected_event+ 1)])
-    
+
     months = ["August", "September", "October", "November", "December", "January", "February", "March", "April", "May", "June", "July" ]
-    
+
 
     list_l = list(df_line['LinkedIn'].iloc[:(selected_event+ 1)])
     list_2 = list(df_line['Newsletter'].iloc[:(selected_event+ 1)])
     list_3 = list(df_line['Instagram'].iloc[:(selected_event+ 1)])
     list_4 = months[:(selected_event + 1)]
-  
-    
+
+
     dict_growth = {
         "LinkedIn": list_l,
         "Mailing list": list_2,
@@ -192,7 +192,7 @@ with col[1]:
         "Month": list_4
     }
     df_com_growth = pd.DataFrame(dict_growth)
-    
+
     fig_line = go.Figure(data=go.Scatter(x=df_com_growth["Month"], y=df_com_growth["LinkedIn"], name="LinkedInS", line_color="#0A66C2",))
     fig_line.add_scatter(x=df_com_growth["Month"], y=df_com_growth["Mailing list"], name="Newsletter", line_color="#F76519")
     fig_line.add_scatter(x=df_com_growth["Month"], y=df_com_growth["Instagram"], name="Instagram", line_color="#E1306C")
@@ -202,14 +202,14 @@ with col[1]:
         tickvals=[0, 1, 2, 3, 4, 5, 6, 7],  # Positions of the ticks on the axis
         ticktext=months  # Labels for the ticks
 ))
-  
+
     st.plotly_chart(fig_line, use_container_width=True,)
-    
+
 
 
 with col[1]:
     st.markdown('<span style="font-size: 30px; color: #4778FF;">Registration Flow</span>', unsafe_allow_html=True)
-     
+
 
     df_event_status = df_event_masked["Attendee Status"].value_counts()
     attended  = df_event_status["Checked In"]
@@ -232,17 +232,17 @@ with col[1]:
     admitted = attended
     # Didn't attend but had a ticket
     no_show = confirmed - admitted
-    
+
     label = ["Registered", "Ticket", "Wait list", "Confirmed", "Cancelled", "Admitted", "No show"]
     source = [0, 0, 1, 1, 2, 3, 3]
     target = [1, 2, 3, 4, 3, 5, 6]
     value = [san_registered, event_ticket_opened, san_wait_list, confirmed, cancelled, admitted, no_show]
 
-    
+
     color_san = ["#00487c","#4bb3fd","#3e6680","#0496ff", "#F82274", "#00FFE1", "#225DFF",]
-    
-   # colors from pie chart -  ["#81D3C1", "#717c89","#8aa2a9","#90baad","#a1e5ab","#adf6b1", "#C1F9C4"] 
-    
+
+   # colors from pie chart -  ["#81D3C1", "#717c89","#8aa2a9","#90baad","#a1e5ab","#adf6b1", "#C1F9C4"]
+
     link= dict(source = source, target = target, value = value, color="#90baad")
     node = dict(label = label, pad = 35, thickness = 10, color=color_san)
     data = go.Sankey(link = link, node = node)
@@ -256,7 +256,7 @@ with col[1]:
     st.plotly_chart(fig_san, use_container_width=True, sharing="streamlit",)
 
 
-    
+
 
 with col[2]:
     df_attendees = pd.DataFrame(df_reshaped["company"].value_counts().reset_index())
